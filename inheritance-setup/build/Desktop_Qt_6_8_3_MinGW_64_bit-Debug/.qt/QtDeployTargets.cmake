e782cd2efc5d113264d362bc5567686200988ec0
Gautam Chaudhri <gautam.chaudhri@gmail.com>
2025-04-11 19:35:18 -0700

    Cleaned up clutter, updated .gitignore

--- inheritance-setup/build/Desktop_Qt_6_8_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_inheritance-setup_FILE C:/Users/minik/Documents/GitHub/CS1C-Class-Project/inheritance-setup/build/Desktop_Qt_6_8_3_MinGW_64_bit-Debug/inheritance-setup.exe)
-set(__QT_DEPLOY_TARGET_inheritance-setup_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_inheritance-setup_RUNTIME_DLLS C:/Qt/6.8.3/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.3/mingw_64/bin/Qt6Core.dll)
